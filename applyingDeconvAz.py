b5b86fe9e0a4242524d17a95dbf5fdbde3f0475c
Adauto Luis <adauto6@netscape.net>
2021-09-15 08:40:34 +0300

    Fix: Foram visto alguns problems nos dados utilizados. os mesmo foram atualizados e para melhorar os resultados, os volumes de 30 e 40 uL foram retirados

--- applyingDeconvAz.py
+++ applyingDeconvAz.py
@@ -12,17 +12,19 @@ from sklearn.metrics import r2_score
 
 df = pd.read_csv('analise.csv')
 
-mu1 = 0.75
+#mu1 = 0.75
+
+mu1 = 0.8
 
 #mu1 = 0.23
 
 sig = 0.05
 
-volumes = [1, 2, 3, 6, 7]
+volumes = [1, 2, 5, 6, 7] # [1, 2, 3, 4, 5, 6, 7]
 
 y_value = [] 
 
-x_value = [0.2, 0.4, 0.6, 1.2, 1.4] # Concentracao Retirada do volume e qtd de azitromicina colocada
+x_value = [0.2, 0.4, 1.0, 1.2, 1.4] # Concentracao Retirada do volume e qtd de azitromicina colocada
 
 for volume in volumes:
 	signalVolu = df[f'Av. I2 {volume}0uL'].values #[100:188]
@@ -41,7 +43,7 @@ for volume in volumes:
 
 	plt.figure(1)
 
-	b, a = signal.butter(4, 0.13, 'low')
+	b, a = signal.butter(4, 0.05, 'low')
 	
 	zi = signal.lfilter_zi(b, a)
 	
@@ -51,7 +53,7 @@ for volume in volumes:
 	
 	signal_t_wo_g1_filter = signal.filtfilt(b, a, signal_t_wo_g1)
 
-#	plt.plot(x, signal_t_wo_g1, label=f'Deconv{volume}0uL')
+	#plt.plot(x, signal_t_wo_g1, label=f'Deconv{volume}0uL')
 	
 	y_value.append(max(z2))
 	
@@ -84,12 +86,14 @@ for volume in volumes:
 
 	signal_conv_t = ifft(signal_conv_f)
 
-	plt.plot(x, signalVolu, label='signal')
+	plt.plot(x, signalVolu, label=f'signal {volume}0uL')
 
 	plt.plot(x, signal_conv_t, label='signal conv t')
 
 
-	plt.legend()
+	plt.legend(bbox_to_anchor=(1.05, 1.0), loc='upper left')
+	plt.tight_layout()
+	
 	if volume >= 4:
 		plt.savefig('signal.png')
 
